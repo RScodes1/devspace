92f4caaa64e7ddfa2b85d53de41cb7cdebf4d9d7
RScodes1 <raj19all@gmail.com>
2025-12-28 02:12:11 +0530

    installing dependencies

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { body } = require("express-validator");
 const { login, signup, refreshToken } = require("../controllers/auth.controller");
 const { validateBody } = require("../middlewares/validate.middleware");
 const { signupSchema, loginSchema } = require("../validations/auth.schema");
